8f0cc49824ee9163b54d1c7027bac1dc94ca5f98
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-05-31 19:22:21 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>PHP PHP PHP</title>
+</head>
+<body>
+      <h1>hello php: Hypertext Preprocessor</h1>
+</body>
+</html>
